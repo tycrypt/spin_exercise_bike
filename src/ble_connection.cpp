c227a8b7f77085a033a6e2940311f74d28278267
tycrypt <100331732+tycrypt@users.noreply.github.com>
2026-07-05 14:16:30 -0500

    accurately parsing characteristic packets and printing via serial

--- src/ble_connection.cpp
+++ src/ble_connection.cpp
@@ -4,6 +4,7 @@
 #include <BLEUtils.h>
 #include "ble_connection.h"
 
+void onDataRecieved (BLERemoteCharacteristic *character, uint8_t* packet_data, size_t packet_size, bool isNotify);
 // STD. bluetooth SIG UUID for cycling speed and cadence
 static BLEUUID serviceUUID((uint16_t)0xFFF0);
 static BLEUUID charUUID((uint16_t)0xFFF1);
@@ -45,7 +46,41 @@ bool connectToDevice(BLEAddress target_device) {
         Serial.print("Error: Could not find 0x2A5B characteristic!");
         return false;
     }
-
+    // register the notify callback function
+    character->registerForNotify(onDataRecieved);
     Serial.printf("Successfully Connected to : %s", target_device.toString().c_str());
     return true;
+}
+
+/* == This func. will act as a callback function to parse bytes that the device is sending
+
+*/
+void onDataRecieved (BLERemoteCharacteristic *character, uint8_t* packet_data, size_t packet_size, bool isNotify) {
+    // Sloop Phase : print the raw hex
+    for ( int i = 0 ; i < packet_size ; i++ ){
+        //print current packet
+        Serial.printf("%02X", packet_data[i]);
+        
+    }
+    Serial.println();
+
+    // ensure that the packet has finished populating
+    if (packet_size >= 11) {
+        /* extract packet_data[2:3] : combine packets 2 and 3
+         data[2] = 0x2A ---> 00000000 00101010
+         data[2] >> 8 = 00101010 00000000
+         data[3] = 0x3B ---> 00000000 00111011
+         data[2] >> 8 | data[3] ---> 00101010 00000000
+                                  OR 00000000 00111011
+                                  ---------------------
+        combined hex bytes           00101010 00111011  
+        */
+
+        uint16_t rawRate = (packet_data[2] << 8) | packet_data[3];
+        float trueRate = rawRate / 5.0;
+        // extract packet_data[9:10] : total revolutions
+        uint16_t totalRevs = (packet_data[10] << 8) | packet_data[9];
+        Serial.printf("Packet Data ---> Live Rate: %.2f || Total Revolutions: %5d\n", trueRate, totalRevs);
+
+    }
 }
